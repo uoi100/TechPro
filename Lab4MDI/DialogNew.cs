aa25b0becd47d7060ffc53b90e3ba0f602ca2d15
uoi100 <calvintruong11@hotmail.com>
2015-02-05 10:19:35 -0800

    Added DialogNew

---
+++ Lab4MDI/DialogNew.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4MDI
+{
+    public partial class DialogNew : Form
+    {
+        Size size1 = new Size(640, 480);
+        Size size2 = new Size(800, 600);
+        Size size3 = new Size(1024, 768);
+        public DialogNew(){
+            InitializeComponent();
+        }
+
+        public Size Resolution{
+            get
+            {
+                if (radioButton1.Checked)
+                    return size1;
+                else if (radioButton2.Checked)
+                    return size2;
+                else
+                    return size3;
+            }
+        }
+    }
+}
