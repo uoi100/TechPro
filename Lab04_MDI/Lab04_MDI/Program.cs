77b8643c97b9dcdf97ad2e25ba29108ae622c193
uoi100 <uo1i00x@gmail.com>
2015-02-05 09:15:21 -0800

    Added Dialogbox, working on form child.

--- Lab04_MDI/Lab04_MDI/Program.cs
+++ Lab04_MDI/Lab04_MDI/Program.cs
@@ -16,7 +16,7 @@ namespace Lab04_MDI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
