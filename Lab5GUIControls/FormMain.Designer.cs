f3a3265237de38d4b1573be44b59f013c34fa591
uoi100 <calvintruong11@hotmail.com>
2015-02-10 12:20:06 -0800

    Implemented majoriy of functionalities, such as entering a directory, moving up one level, opening a file, changing the views to tile, list, small icons, large icons. Single clicking on a folder will change directories, double clicking on a file will open that file.

---
+++ Lab5GUIControls/FormMain.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Lab5GUIControls
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.text_Path = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.worker_Progress = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileDefaultToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smallIconsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.largeIconsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(3, 48);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(573, 200);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // text_Path
+            // 
+            this.text_Path.AutoSize = true;
+            this.text_Path.Location = new System.Drawing.Point(3, 235);
+            this.text_Path.Name = "text_Path";
+            this.text_Path.Size = new System.Drawing.Size(35, 13);
+            this.text_Path.TabIndex = 1;
+            this.text_Path.Text = "Path: ";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 278);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(570, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // worker_Progress
+            // 
+            this.worker_Progress.WorkerReportsProgress = true;
+            this.worker_Progress.DoWork += new System.ComponentModel.DoWorkEventHandler(this.worker_Progress_DoWork);
+            this.worker_Progress.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.worker_Progress_ProgressChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(588, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tileDefaultToolStripMenuItem,
+            this.listToolStripMenuItem,
+            this.smallIconsToolStripMenuItem,
+            this.largeIconsToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // tileDefaultToolStripMenuItem
+            // 
+            this.tileDefaultToolStripMenuItem.Name = "tileDefaultToolStripMenuItem";
+            this.tileDefaultToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.tileDefaultToolStripMenuItem.Text = "Tile (Default)";
+            this.tileDefaultToolStripMenuItem.Click += new System.EventHandler(this.tileDefaultToolStripMenuItem_Click);
+            // 
+            // listToolStripMenuItem
+            // 
+            this.listToolStripMenuItem.Name = "listToolStripMenuItem";
+            this.listToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.listToolStripMenuItem.Text = "List";
+            this.listToolStripMenuItem.Click += new System.EventHandler(this.listToolStripMenuItem_Click);
+            // 
+            // smallIconsToolStripMenuItem
+            // 
+            this.smallIconsToolStripMenuItem.Name = "smallIconsToolStripMenuItem";
+            this.smallIconsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.smallIconsToolStripMenuItem.Text = "Small Icons";
+            this.smallIconsToolStripMenuItem.Click += new System.EventHandler(this.smallIconsToolStripMenuItem_Click);
+            // 
+            // largeIconsToolStripMenuItem
+            // 
+            this.largeIconsToolStripMenuItem.Name = "largeIconsToolStripMenuItem";
+            this.largeIconsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.largeIconsToolStripMenuItem.Text = "Large Icons";
+            this.largeIconsToolStripMenuItem.Click += new System.EventHandler(this.largeIconsToolStripMenuItem_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 313);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.text_Path);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label text_Path;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker worker_Progress;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileDefaultToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem smallIconsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem largeIconsToolStripMenuItem;
+    }
+}
+
---
+++ Lab5GUIControls/FormMain.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace Lab5GUIControls
+{
+    public partial class FormMain : Form
+    {
+        private System.IO.DirectoryInfo currentDirectory;
+        public FormMain()
+        {
+            InitializeComponent();
+
+            ImageList imageList = new ImageList();
+
+            imageList.Images.Add("file", Lab5GUIControls.Properties.Resources.file);
+            imageList.Images.Add("folder", Lab5GUIControls.Properties.Resources.folder);
+            listView1.LargeImageList = imageList;
+            listView1.SmallImageList = imageList;
+
+            testList();
+        }
+
+        /// <summary>
+        /// Description: Gets the files and folders at the root of the folder where this program is held at.
+        /// </summary>
+        private void testList()
+        {
+            currentDirectory = new System.IO.DirectoryInfo(System.IO.Directory.GetDirectoryRoot(System.IO.Directory.GetCurrentDirectory()));
+
+            FileInfo[] files = currentDirectory.GetFiles();
+
+            //retrieve all image files
+            foreach(FileInfo file in files)
+            {
+                //listView1.Items.Add(files[i]);
+                var listViewItem = listView1.Items.Add(file.FullName);
+                listViewItem.ImageKey = "file";
+            }
+
+            DirectoryInfo[] folders = currentDirectory.GetDirectories();
+
+            foreach (DirectoryInfo folder in folders)
+            {
+                var listViewItem = listView1.Items.Add(folder.FullName);
+                listViewItem.ImageKey = "folder";
+            }
+
+            text_Path.Text = "Path: " + currentDirectory.FullName;
+        }
+
+        /// <summary>
+        /// Description: Change the current directory to the specified path
+        /// </summary>
+        /// <param name="path"></param>
+        private void changeDirectory(string path)
+        {
+            listView1.Clear();
+
+            currentDirectory = new System.IO.DirectoryInfo(path);
+            FileInfo[] files = currentDirectory.GetFiles();
+            DirectoryInfo[] folders = currentDirectory.GetDirectories();
+
+            if (currentDirectory.Parent != null)
+            {
+                ListViewItem item = listView1.Items.Add("Go Up One Level");
+                item.ImageKey = "...";
+            }
+
+            foreach (FileInfo file in files)
+            {
+                var listViewItem = listView1.Items.Add(file.FullName);
+                listViewItem.ImageKey = "file";
+            }
+
+            foreach (DirectoryInfo folder in folders)
+            {
+                var listViewItem = listView1.Items.Add(folder.FullName);
+                listViewItem.ImageKey = "folder";
+            }
+
+        }
+
+        private void listView1_ItemActivate(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection items = listView1.SelectedItems;
+
+            try
+            {
+                foreach (ListViewItem item in items)
+                {
+                    if( item.ImageKey == "file" )
+                        Process.Start(item.Text);
+                }
+
+                text_Path.Text = "Path: " + currentDirectory.FullName;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void worker_Progress_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                worker_Progress.ReportProgress(i);
+            }
+        }
+
+        private void worker_Progress_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection items = listView1.SelectedItems;
+
+            try
+            {
+                foreach (ListViewItem item in items)
+                {
+                    if (item.ImageKey == "...")
+                    {
+
+                        if (currentDirectory.Parent != null)
+                        {
+                            progressBar1.Style = ProgressBarStyle.Continuous;
+                            progressBar1.Maximum = 100;
+                            progressBar1.Value = 0;
+                            progressBar1.Step = 1;
+                            worker_Progress.RunWorkerAsync();
+                            changeDirectory(currentDirectory.Parent.FullName);
+                        }
+                    }
+                    else if (item.ImageKey == "folder")
+                    {
+                        progressBar1.Style = ProgressBarStyle.Continuous;
+                        progressBar1.Maximum = 100;
+                        progressBar1.Value = 0;
+                        progressBar1.Step = 1;
+                        worker_Progress.RunWorkerAsync();
+                        changeDirectory(item.Text);
+                    }
+                }
+
+                text_Path.Text = "Path: " + currentDirectory.FullName;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.List;
+        }
+
+        private void tileDefaultToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.Tile;
+        }
+
+        private void smallIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.SmallIcon;
+        }
+
+        private void largeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.LargeIcon;
+        }
+
+        // End of Class
+    }
+}
