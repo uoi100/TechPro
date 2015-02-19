c3a26aa6033722d3f10c1acc075becbbf06ba6f1
uoi100 <calvintruong11@hotmail.com>
2015-02-19 09:45:57 -0800

    Lab6_FirstPart

---
+++ Lab6TeamActivity/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6TeamActivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "CST Student";
+            string name2 = name;
+            name = "Techpro Student";
+            Console.WriteLine(name2);
+            Console.Read();
+        }
+    }
+}
